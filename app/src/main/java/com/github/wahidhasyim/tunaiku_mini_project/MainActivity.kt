6c0be217e077582e779647f49c69ef9628741284
wahidhasyim <hasyimwahid694@gmail.com>
2021-12-06 13:26:56 +0700

    initial commit

---
+++ app/src/main/java/com/github/wahidhasyim/tunaiku_mini_project/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.wahidhasyim.tunaiku_mini_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
